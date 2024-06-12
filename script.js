bcc04641c66c668aef18b0a0a8bd82e3e888e94d
devopashiq <168412616+devopashiq@users.noreply.github.com>
2024-06-12 15:07:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let input="";
 
+
+
+function appendToDisplay(number){
+    input +=number;
+   document.getElementById("display").value =input;
+    
+    
+    
+}
+
+function calculate(){
+ try{
+    let result =eval(input);
+    document.getElementById("display").value= result;
+    input= result.toString();
+ }catch(e){
+    document.getElementById("display").value="Error";
+    input="";
+ }
+}
+
+function clearDisplay(){
+    input="";
+    document.getElementById("display").value =input;
+}
